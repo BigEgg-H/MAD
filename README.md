28926a8392335a362262cc3bc6372e9e55a029bf
BigEgg-H <1515015820@qq.com>
2024-06-19 18:07:05 +0800

    添加项目文件。

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MAD
+## This repository is for Marisa's Atelier of Danmaku. 
+
---
+++ test/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "../mad_source/mad_include/mad.h"
+#include "../mad_source/template/mad_templates.h"
+
+int main() {
+
+	std::cout << "hello world.";
+
+	return 0;
+}
