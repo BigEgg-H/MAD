d27cd7f752f685e1ccc8742b20401cef8cf43851
AnEgg <1515015820@qq.com>
2024-12-10 16:30:02 +0800

    添加快速调用Lua函数功能
    增加了快速调用Lua函数的相关方法，包括注册、调用和取消注册快速调用包，以提高大量参数函数调用的性能。同时更新了测试代码，对比了不同调用方式的性能差异。

--- TestApp/main.cpp
+++ TestApp/main.cpp
@@ -1,6 +1,7 @@
 #include "../MAD/mad.h"
 
 #include <iostream>
+#include <chrono>
 using namespace std;
 
 void test_err_printer(const MADString& _str) {
@@ -22,6 +23,9 @@ int add(lua_State* _L)
 	return 1;
 }
 
+#define TIME_POINT_START {auto start = std::chrono::high_resolution_clock::now();
+#define TIME_POINT_END auto finish = std::chrono::high_resolution_clock::now(); std::chrono::duration<double> elapsed = finish - start; MAD_LOG_INFO("TimePoint: " + to_string(elapsed.count()) + "s");}
+
 int main() {
 	/*Debugger testing*/
 	MAD_Debugger::GetInstance().SetPrinter(PrinterType::Error, test_err_printer);
@@ -29,16 +33,36 @@ int main() {
 	MAD_Debugger::GetInstance().SetPrinter(PrinterType::Infomation, test_info_printer);
 
 	/*Script testing*/
-	MADScript* mad_script = MADScript::CreateScript("print(add())\nprint(str)\nCopyData(test_buffer,true)");
-	mad_script->RegisterCFunction("add",add);
-	mad_script->SetValueString("str", "[MAD_TAPP_INFO]: TEST-Set globe value ............OK");
-	bool* test_buffer = new bool(false);
-	mad_script->SetValueUserPtr("test_buffer", test_buffer);
-	mad_script->RunDirectly();
-	
-	if (*test_buffer)
-	{
-		MAD_LOG_INFO("TEST-Copy user data ............OK");
-	}
+	MADScript* mad_script = MADScript::CreateScript("function a(b,c)\n\taaa = 1\n\tbbb = aaa + 10\nend");
+	if (!mad_script)
+		return 1;
 
+	MADScriptDataStream arg_data = MADScriptDataStream();
+	arg_data.push_back(MADScriptData(MADScriptValueType::String, new MADString("Hello args!")));
+	arg_data.push_back(MADScriptData(MADScriptValueType::String, new MADString("yessssssssssssssssssssssssssssssssssssssssssss")));
+	mad_script->RunDirectly();
+	MADQCPack pack = mad_script->RegisterQuickCallPack("a",arg_data);
+	const size_t allcount = 1000000;
+	MAD_LOG_INFO("CallFunction");
+	TIME_POINT_START
+	for(size_t i = 0; i < allcount; i++)
+	{
+		mad_script->CallFunction("a",arg_data);
+	}
+	TIME_POINT_END
+	MAD_LOG_INFO("QuickCallFunction");
+	TIME_POINT_START
+	for(size_t i = 0; i < allcount; i++)
+	{
+		mad_script->QuickCallFunction(pack);
+	}
+	TIME_POINT_END
+	MAD_LOG_INFO("UnsafeFastCallFunction");
+	TIME_POINT_START
+	for(size_t i = 0; i < allcount; i++)
+	{
+		mad_script->UnsafeFastCallFunction("a");
+	}
+	TIME_POINT_END
+	mad_script->UnregisterQuickCallPack(pack);
 }
